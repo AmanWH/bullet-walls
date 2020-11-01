1c73d7196744250e8e844325377f802758dbd953
greatest coder <73113014+AmanWH@users.noreply.github.com>
2020-11-01 19:18:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
 
   if(wall.x-bullet.x < (bullet.width + wall.width)/2){
     bullet.velocityX=0;
-    var damage=0.5 * weight * speed * speed/thickness * thickness * thickness;
+    var damage=(0.5 * weight * speed * speed)/(thickness * thickness * thickness);
 
     if(damage < 10){
       wall.shapeColor=("green");
